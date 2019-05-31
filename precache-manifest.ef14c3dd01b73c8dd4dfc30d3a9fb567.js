0cc572e332e0c80c95d2010885ea50cce5b41559
candye_sweet <olya.melska@gmail.com>
2019-05-31 16:28:59 -0400

    Updates

---
+++ precache-manifest.ef14c3dd01b73c8dd4dfc30d3a9fb567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ac031cca4b93e458e8a5afd0ee7094",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "caae3beacaed704b0f0f",
+    "url": "/clicky-game/static/css/main.5b539cb7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "caae3beacaed704b0f0f",
+    "url": "/clicky-game/static/js/main.9545e796.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "47eebcb110739a41687bee75be6aa013",
+    "url": "/clicky-game/static/media/header.47eebcb1.jpg"
+  }
+]);
