cecc3432675d8367c87f0732162bd2f6ae4ce8c3
candye_sweet <olya.melska@gmail.com>
2019-05-31 20:54:06 -0400

    Adding @media

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "../styles/navbar.css";
 const Navbar = props => {
   return (
     <nav id="nav" className="sticky-top ">
-      <h2>Click on ART!</h2>
+      <h2 id="art">Click on ART!</h2>
       <h2 id="text">{props.text}</h2>
       <h2 id="score">Score: {props.score}</h2>
       <h2 id="total-score">Highest Score: {props.bestScore}</h2>
